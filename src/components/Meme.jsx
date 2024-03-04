87b29dad6ac757bedeb23bd62cb6573557bf658d
VincenzoCastrovilli <vcastrovilli@icloud.com>
2024-03-04 20:54:04 +0100

    Add labels to form inputs

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,20 +4,24 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input
-          type="text"
-          name=""
-          id=""
-          className="form-input"
-          placeholder="Top Text"
-        />
-        <input
-          type="text"
-          name=""
-          id=""
-          className="form-input"
-          placeholder="Bottom Text"
-        />
+        <div>
+          <label htmlFor="top-text">Top Text</label>
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="bottom-text">Bottom text</label>
+          <input
+            type="text"
+            placeholder="and take my money"
+            className="form-input"
+            id="bottom-text"
+          />
+        </div>
         <button className="form-button">Get a new meme image</button>
       </form>
     </main>
